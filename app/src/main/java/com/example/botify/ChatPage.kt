ab3eb73249c0b34f52fe44d70d9a74fb40bda456
JB <jeevajb163@gmail.com>
2025-02-04 17:16:18 +0530

    Status bar color changed as of app bar color

--- app/src/main/java/com/example/botify/ChatPage.kt
+++ app/src/main/java/com/example/botify/ChatPage.kt
@@ -1,5 +1,7 @@
 package com.example.botify
 
+import android.app.Activity
+import android.os.Build
 import android.widget.Toast
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,6 +13,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -19,7 +22,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.botify.ui.theme.BotifyTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.draw.clip
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 @Composable
 fun ChatPage(modifier: Modifier) {
@@ -28,19 +34,33 @@ fun ChatPage(modifier: Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBar() {
-    val cntxt = LocalContext.current.applicationContext
+    val a = LocalContext.current.applicationContext
+
+    val context = LocalContext.current
+    val activity = context as Activity
+    val toAppBarColor = MaterialTheme.colorScheme.tertiary
+    val view = LocalView.current
+    SideEffect{
+        val window = activity.window
+        val insetsController = WindowInsetsControllerCompat(window,view)
+        WindowCompat.getInsetsController(window,view).isAppearanceLightStatusBars = false
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            insetsController.isAppearanceLightStatusBars = false
+        }
+        window.decorView.setBackgroundColor(toAppBarColor.toArgb())
+    }
+
     TopAppBar(title = { Text("Botify", fontSize = 24.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.SemiBold) },
         navigationIcon = {
-            IconButton(onClick = { Toast.makeText(cntxt, "Feature not found. Even AI has its lazy days!", Toast.LENGTH_LONG).show()}) {
+            IconButton(onClick = { Toast.makeText(a, "Feature not found. Even AI has its lazy days!", Toast.LENGTH_LONG).show()}) {
                 Icon(painter = painterResource(R.drawable.nav_icon),
                     contentDescription = null, modifier = Modifier.size(200.dp))
             }
         }, colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.tertiary,
+            containerColor = toAppBarColor,
             titleContentColor = MaterialTheme.colorScheme.onTertiary,
             navigationIconContentColor = MaterialTheme.colorScheme.onTertiary
-        ),
-        modifier = Modifier.clip(MaterialTheme.shapes.medium))
+        ))
 }
 
 @Preview(showBackground = true, showSystemUi = true)
--- app/src/main/java/com/example/botify/MainActivity.kt
+++ app/src/main/java/com/example/botify/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.example.botify.ui.theme.BotifyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             BotifyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize().statusBarsPadding().navigationBarsPadding()) {
