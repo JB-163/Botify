ef135b8ec9fe0571f406926d79eb5e8ae1c43b63
JB <jeevajb163@gmail.com>
2025-02-16 12:31:57 +0530

    Chat history sent to API

--- app/src/main/java/com/example/botify/ChatViewModel.kt
+++ app/src/main/java/com/example/botify/ChatViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.botify
 
 import android.annotation.SuppressLint
+import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.Content
+import com.google.ai.client.generativeai.type.content
 import kotlinx.coroutines.launch
 
 class ChatViewModel : ViewModel() {
@@ -23,7 +26,14 @@ class ChatViewModel : ViewModel() {
 
     fun sendMessage(question : String) {
         viewModelScope.launch {
-            val chat = generativeModel.startChat()
+            val chat = generativeModel.startChat(
+                // Code for saving chat history to API
+                history = messageList.map{
+                    content(it.role) {
+                        text(it.message)
+                    }
+                }.toList()
+            )
 
             // Adding user message to the messageList
             messageList.add(MessageModel(message = question, role = "user"))
