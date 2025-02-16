56fd8e58c420433203c5b2f21b3cdae159c9b95d
JB <jeevajb163@gmail.com>
2025-02-16 12:43:59 +0530

    Handled with internet error

--- app/src/main/java/com/example/botify/ChatViewModel.kt
+++ app/src/main/java/com/example/botify/ChatViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.botify
 
 import android.annotation.SuppressLint
-import androidx.compose.material3.Text
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
-import com.google.ai.client.generativeai.type.Content
 import com.google.ai.client.generativeai.type.content
 import kotlinx.coroutines.launch
 
@@ -24,22 +24,37 @@ class ChatViewModel : ViewModel() {
         apiKey = Constants.API_KEY
     )
 
+    @RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
     fun sendMessage(question : String) {
         viewModelScope.launch {
-            val chat = generativeModel.startChat(
-                // Code for saving chat history to API
-                history = messageList.map{
-                    content(it.role) {
-                        text(it.message)
-                    }
-                }.toList()
-            )
 
-            // Adding user message to the messageList
-            messageList.add(MessageModel(message = question, role = "user"))
-            val response = chat.sendMessage(question)
-            // Adding model response to the messageList
-            messageList.add(MessageModel(message = response.text.toString(),role = "model"))
+            try {
+                val chat = generativeModel.startChat(
+                    // Code for saving chat history to API
+                    history = messageList.map{
+                        content(it.role) {
+                            text(it.message)
+                        }
+                    }.toList()
+                )
+
+                // Adding user message to the messageList
+                messageList.add(MessageModel(message = question, role = "user"))
+
+                // Fake message added to show typing
+                messageList.add(MessageModel("Typing...", role = "model"))
+                val response = chat.sendMessage(question)
+                // Removing fake message
+                messageList.removeLast()
+
+                // Adding model response to the messageList
+                messageList.add(MessageModel(message = response.text.toString(),role = "model"))
+            }
+
+            catch (e : Exception) {
+                messageList.removeLast()
+                messageList.add(MessageModel("Error : ${e.message.toString()}", role = "model"))
+            }
         }
     }
 }
